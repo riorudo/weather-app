79427ef6415ced156ecbd6f113c04326a36d0d4f
Michael Hochwieser <michael.hochwieser@gmail.com>
2020-05-01 18:16:50 +0200

    Create server side api to get data from openweathermap.org.

--- server.js
+++ server.js
@@ -2,18 +2,54 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 3000;
+const server = app.listen(port, function() {
+    console.log(`Local node.js server is running on port: ${port}!!!`);
+});
+
+
+//Endpoints
+const axios = require("axios");
+const basicUrl = "https://api.openweathermap.org/data/2.5/weather?";
+const apiKey = "appid=6ad16bfda5278e2abe721759f54554ed";
+
+const getWeatherData = async (url = '') => {
+    try {
+        const res = await axios.get(url);
+        const data = res.data;
+        return data;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+app.get('/zip', async function (req, res) {
+    const url = `${basicUrl}q=${req.query.zip},${req.query.countryCode}&${apiKey}`;
+    res.send(await getWeatherData(url));
+})
+
+app.get('/coordinates', async function (req, res) {
+    const url = `${basicUrl}lat=${req.query.lat}&lon=${req.query.lon}&${apiKey}`;
+    res.send(await getWeatherData(url));
+})
+
--- website/app.js
+++ website/app.js
@@ -2,4 +2,48 @@
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+
+const getWeatherData = async (url = '') => {
+    const res = await fetch(url, {
+        method: 'GET',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
+    try {
+        return await res.json();
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const url = `/coordinates?lat=${position.coords.latitude}&lon=${position.coords.longitude}`;
+            getWeatherData(url)
+                .then((data) => {
+                    console.log('data by coordinates');
+                    console.log(data);
+                });
+        });
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+
+setTimeout(() => {
+    const url = `/zip?zip=8046&countryCode=ch`
+    getWeatherData(url)
+        .then((data) => {
+            console.log('data by zip');
+            console.log(data);
+        });
+
+    getLocation();
+}, 0);
